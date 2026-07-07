8617ce12d54f1e14f0a89ff689b5a5bcd35834b1
yoha <yohaseelan@outlook.com>
2026-07-07 20:02:00 +0530

    Serve static pages; add HTML and assets
    Add pages and public assets and update app.js to serve them. app.js now requires path, uses express.static for public/, and serves routes with res.sendFile. New files: pages/index.html, about.html, contact.html, profile.html, myinfo.html; public/css/style.css; public/images/yarlit-logo.jpg. Enables serving HTML pages, CSS and images from the public folder and links routes from the index page.

--- app.js
+++ app.js
@@ -1,27 +1,29 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 const port = 5000;
+app.use(express.static(path.join(__dirname, 'public')));
 const mypage = (req, res) => {
-    res.send('<h1 style="color: blue;">Welcome to my page</h1>');
+    console.log(path.join(__dirname, 'pages', 'index.html'));
+    res.sendFile(path.join(__dirname, 'pages', 'index.html'));
 };
 app.get('/', mypage);
 
 app.get('/about', (req, res) => {
-    let myname = "seelan";
-    res.send(`<h1 style="color: green;">About Page</h1><p>Hello, my name is ${myname}.</p>`);
+    res.sendFile(path.join(__dirname, 'pages', 'about.html'))
 });
 app.get('/contact', (req, res) => {
-    res.send('<h1 style="color: red;">Contact Page</h1>');
+    res.sendFile(path.join(__dirname, 'pages', 'contact.html'))
 });
 app.get('/profile', (req, res) => {
     let myname = req.query.name || "Guest";
     let myage = req.query.age || "unknown";
-    res.send(`<h1 style="color: red;">Profile Page</h1><p>Hello, my name is ${myname} and I am ${myage} years old.</p>`);
+    res.sendFile(path.join(__dirname, 'pages', 'profile.html'))
 });
 app.get('/myinfo/:name/:age', (req, res) => {
     let myname = req.params.name || "Guest";
     let myage = req.params.age || "unknown";
-    res.send(`<h1 style="color: red;">my info Page</h1><p>Hello, my name is ${myname} and I am ${myage} years old.</p>`);
+    res.sendFile(path.join(__dirname, 'pages', 'myinfo.html'))
 });
 
 app.listen(port, () => {
