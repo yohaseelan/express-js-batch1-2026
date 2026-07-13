36b952d44f96f1d59632b0e0d9d04458652e82c2
yoha <yohaseelan@outlook.com>
2026-07-13 20:06:33 +0530

    Refactor app to use EJS templating
    Switch from serving static files to using EJS view engine for dynamic template rendering. Removed unused routes and middleware (path, method-override). Simplified student routes to render templates with dynamic data. Added three EJS view templates: index, student/index, and student/show.

--- app.js
+++ app.js
@@ -1,114 +1,20 @@
 const express = require('express');
-const path = require('path');
-const methodOverride = require("method-override");
+
 const app = express();
 const port = 5000;
+app.set('view engine', 'ejs');
 
-app.use(express.static(path.join(__dirname, 'public')));
-app.use(express.urlencoded({ extended: true }));
-app.use(methodOverride('_method'));
-const mypage = (req, res) => {
-    console.log(path.join(__dirname, 'pages', 'index.html'));
-    res.sendFile(path.join(__dirname, 'pages', 'index.html'));
-};
-app.get('/', mypage);
-
-app.get('/about', (req, res) => {
-
-    res.sendFile(path.join(__dirname, 'pages', 'about.html'))
+app.get('/', (req, res) => {
+    res.render('index', { title: 'Hey', message: 'Hello there!' });
 });
-app.get('/contact', (req, res) => {
-    res.sendFile(path.join(__dirname, 'pages', 'contact.html'))
-});
-app.get('/profile', (req, res) => {
 
-    let myname = req.query.name || "Guest";
-    let myage = req.query.age || "unknown";
-    res.sendFile(path.join(__dirname, 'pages', 'profile.html'))
-});
-app.get('/myinfo/:name/:age', (req, res) => {
-    let myname = req.params.name || "Guest";
-    let myage = req.params.age || "unknown";
-    res.sendFile(path.join(__dirname, 'pages', 'myinfo.html'))
-});
-app.get('/download', (req, res) => {
-
-    // res.download(path.join(__dirname, 'files', 'doc-2.docx'), 'seelan.docx', (err) => {
-    //     if (err) {
-    //         console.log(err);
-    //         res.status(404).send('Error downloading file');
-    //     } else {
-
-    //         console.log('File downloaded successfully');
-    //         res.end();
-    //     }
-    // });
-    res.download(path.join(__dirname, 'files', 'doc-2.docx'), (err) => {
-        if (err) {
-            console.log(err);
-            res.status(404).send('Error downloading file');
-        } else {
-
-            console.log('File downloaded successfully');
-
-        }
-    });
-});
-app.get('/download-pdf', (req, res) => {
-
-    res.sendFile(path.join(__dirname, 'files', '7.pdf'), (err) => {
-        if (err) {
-            console.log(err);
-            res.status(404).send('Error downloading file');
-        } else {
-
-            console.log('File downloaded successfully');
-            res.end();
-        }
-    });
-});
-app.get('/photo', (req, res) => {
-
-    res.download(path.join(__dirname, 'files', 'yarlit.jpg'), (err) => {
-        if (err) {
-            console.log(err);
-            res.status(404).send('Error downloading file');
-        } else {
-
-            console.log('File downloaded successfully');
-            res.end();
-        }
-    });
-});
-app.put('/submit', (req, res) => {
-    let fname = req.body.fname || "Guest";
-    let lname = req.body.lname || "unknown";
-
-    res.send(`Form submitted successfully for ${fname} ${lname}`);
-
-});
 app.get('/student', (req, res) => {
-
-  res.send("show student details");
+    res.render('student/index', { title: 'Student Page', message: 'Welcome to the student page!' });
 });
 app.get('/student/:id', (req, res) => {
-   let studentId = req.params.id;
-  res.send(`show student details for student with ID: ${studentId}`);
-
+    const studentId = req.params.id;
+    res.render('student/show', { title: 'Student Page', message: `Welcome to the student page for student ID: ${studentId}` });
 });
-app.post('/student', (req, res) => {
-  res.send("create a new student");
-});
-app.put('/student/:id', (req, res) => {
-  let studentId = req.params.id;
-  res.send(`update student details for student with ID: ${studentId}`);
-}); 
-app.delete('/student/:id', (req, res) => {
-  let studentId = req.params.id;
-  res.send(`delete student with ID: ${studentId}`);
-});
-
-
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`);
 });
