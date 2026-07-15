351cdc950bc77fa49af4298c73e54210ff5d5af4
yoha <yohaseelan@outlook.com>
2026-07-15 19:55:54 +0530

    Add master layout template for student page
    Implement a master EJS layout template for consistent page structure. Configure the /student route to use the new master layout which provides base HTML structure with a body placeholder for nested views.

--- app.js
+++ app.js
@@ -11,12 +11,13 @@ app.get('/', (req, res) => {
 
 
 app.get('/student', (req, res) => {
-    res.render('student/index', { title: 'Student Page', message: 'Welcome to the student page!' });
+    res.render('student/index', {layout: 'layouts/master', title: 'Student Page', message: 'Welcome to the student page!' });
 });
 app.get('/student/:id/show', (req, res) => {
     const studentId = req.params.id;
     res.render('student/show', { title: 'Student show Page', message: `Welcome to the student page for student ID: ${studentId}` });
 });
+
 app.get('/student/create', (req, res) => {
     res.render('student/create', { title: 'Student Create Page', message: 'Welcome to the student create page!' });
 });
