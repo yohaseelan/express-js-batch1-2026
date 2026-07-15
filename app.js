d7a57de6b9a3470371dbda5483e1ae2236fe7f27
yoha <yohaseelan@outlook.com>
2026-07-15 19:45:00 +0530

    Add layout system and student create route
    Implement express-ejs-layouts to establish a consistent layout across pages. Refactor existing views to use the new master layout, eliminating duplicate HTML boilerplate. Add new student create route and form. Update port from 5000 to 3000 and restructure student show route to /student/:id/show.

--- app.js
+++ app.js
@@ -1,19 +1,24 @@
 const express = require('express');
-
+const layout = require('express-ejs-layouts');
 const app = express();
-const port = 5000;
+const port = 3000;
 app.set('view engine', 'ejs');
-
+app.use(layout);
+app.set('layout', 'layouts/main');
 app.get('/', (req, res) => {
-    res.render('index', { title: 'Hey', message: 'Hello there!' });
+    res.render('index', { title: 'home', message: 'Hello there!' });
 });
+
 
 app.get('/student', (req, res) => {
     res.render('student/index', { title: 'Student Page', message: 'Welcome to the student page!' });
 });
-app.get('/student/:id', (req, res) => {
+app.get('/student/:id/show', (req, res) => {
     const studentId = req.params.id;
-    res.render('student/show', { title: 'Student Page', message: `Welcome to the student page for student ID: ${studentId}` });
+    res.render('student/show', { title: 'Student show Page', message: `Welcome to the student page for student ID: ${studentId}` });
+});
+app.get('/student/create', (req, res) => {
+    res.render('student/create', { title: 'Student Create Page', message: 'Welcome to the student create page!' });
 });
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`);
